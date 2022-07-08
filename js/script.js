596dbca5f22f1fcfa2c74443ef7352bff255e2ae
sarah <skg@neofilm.us>
2022-07-07 20:27:59 -0400

    Create an Object & Properties with Dot Notation

--- js/script.js
+++ js/script.js
@@ -1,2 +1,25 @@
 const topicElement = document.querySelector(".topic");
 const countElement = document.querySelector(".count");
+
+// create new variable & assign it an empty object
+const learning = {};
+
+// create a property of learning object with a value of "JS"
+learning.topic = "JS";
+
+// create another object property & assign it an array that contains four learning goals as elements
+learning.learningGoals = [
+    "Understand programming fundamentals",
+    "Learn something new",
+    "Have Fun",
+    "Build JS programs"
+  ];
+
+  // create an object property called category with a value
+  learning.category = "Front End Development";
+
+  // create another object property called topicImportance & give it a value of "High"
+  learning.topicImportance = "High";
+
+  console.log(learning);
+
