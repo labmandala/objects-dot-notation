7dc6b29007aefd38de120ce2bddcf801853f2b6d
sarah <skg@neofilm.us>
2022-07-16 15:04:40 -0400

    Refactor code to be more compact, object-literal

--- js/script.js
+++ js/script.js
@@ -1,48 +1,24 @@
 const topicElement = document.querySelector(".topic");
 const countElement = document.querySelector(".count");
+const body = document.querySelector("body");
+const success = document.querySelector(".success");
+const dance = document.querySelector(".dance");
 
-// create new variable & assign it an empty object
-const learning = {};
-
-// create a property of learning object with a value of "JS"
-learning.topic = "JS";
-
-// create another object property & assign it an array that contains four learning goals as elements
-learning.learningGoals = [
+const learning = {
+  topic: "JS",
+  learningGoals: [
     "Understand programming fundamentals",
-    "Learn something new",
     "Have Fun",
     "Build JS programs"
-];
+  ],
+  category: "Front End Development",
+  topicImportance: "high",
+  hoursThisWeek: 2,
+  weeklyHourGoal: 7
+};
 
-// create an object property called category with a value
-learning.category = "Front End Development";
-
-// create another object property called topicImportance & give it a value of "High"
-learning.topicImportance = "High";
-
-console.log(learning);
-
-// change value of the topic property
-learning.topic = "JavaScript";
-
-// remove second item from array
-learning.learningGoals.splice(1, 1);
-
-// log out object to check changes
-console.log(learning);
-
-/* set value of topicElement variable (using innerText property) to string 
-with value of topic property using template literals and dot notation */
 topicElement.innerText = `✔️ I'm learning ${learning.topic}.`;
-
-// remove class “hide” that’s given to the topicElement
-topicElement.classList.remove("hide");
-
-/* set the innerText of the countElement to string
-with the length of the learningGoals property array */
 countElement.innerText = `✔️ I have ${learning.learningGoals.length} learning goals.`;
 
-// remove the class “hide” that’s given to countElement
+topicElement.classList.remove("hide");
 countElement.classList.remove("hide");
-
