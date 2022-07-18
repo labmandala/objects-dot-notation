5e927c9857172b2f105e2addd139d8acced8c863
sarah <skg@neofilm.us>
2022-07-17 20:12:55 -0400

    Create new properties, method

--- js/script.js
+++ js/script.js
@@ -1,20 +1,26 @@
 const topicElement = document.querySelector(".topic");
 const countElement = document.querySelector(".count");
+// new DOM elements selected for styles
 const body = document.querySelector("body");
 const success = document.querySelector(".success");
 const dance = document.querySelector(".dance");
 
+// learning object is now an object literal
+// create new properties, make method
 const learning = {
   topic: "JS",
   learningGoals: [
     "Understand programming fundamentals",
     "Have Fun",
-    "Build JS programs"
+    "Build JS programs",
   ],
   category: "Front End Development",
   topicImportance: "high",
   hoursThisWeek: 2,
-  weeklyHourGoal: 7
+  weeklyHourGoal: 7,
+  achievedStudyGoal: false,
+  addStudyTime: function (hours) {
+  }
 };
 
 topicElement.innerText = `✔️ I'm learning ${learning.topic}.`;
