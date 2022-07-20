f479d7643bb6a3e466fd6dbbb8cb84d8a5f2fa0b
sarah <skg@neofilm.us>
2022-07-20 18:13:57 -0400

    Call methods, update styles

--- js/script.js
+++ js/script.js
@@ -1,39 +1,41 @@
 const topicElement = document.querySelector(".topic");
 const countElement = document.querySelector(".count");
-// new DOM elements selected for styles
 const body = document.querySelector("body");
 const success = document.querySelector(".success");
 const dance = document.querySelector(".dance");
 
-// learning object is now an object literal
-// create new properties, make method
 const learning = {
   topic: "JS",
   learningGoals: [
     "Understand programming fundamentals",
     "Have Fun",
-    "Build JS programs",
+    "Build JS programs"
   ],
   category: "Front End Development",
   topicImportance: "high",
   hoursThisWeek: 2,
   weeklyHourGoal: 7,
   achievedStudyGoal: false,
-  addStudyTime: function (hours) { 
+  addStudyTime: function (hours) {
     this.hoursThisWeek += hours;
     if (this.hoursThisWeek >= this.weeklyHourGoal) {
-        this.achievedStudyGoal = true;
-    };
+      this.achievedStudyGoal = true;
+      this.celebrate();
+    }
   },
   celebrate: function () {
     body.classList.add("celebrate");
     success.classList.remove("hide");
-    dance.classList.remove("hide"); 
+    dance.classList.remove("hide");
   }
 };
+
+console.log(learning);
 
 topicElement.innerText = `✔️ I'm learning ${learning.topic}.`;
 countElement.innerText = `✔️ I have ${learning.learningGoals.length} learning goals.`;
 
 topicElement.classList.remove("hide");
 countElement.classList.remove("hide");
+
+learning.addStudyTime(5);
